2fa83dbf2ddb51e518d1a4b9a458e0e440dd9b01
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 15:40:01 -0500

    chore: import

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const TOKEN_COLOR = 'green';
+export const TOKEN_SIZE = '13px';
+export const TOKEN_WEIGHT = 'bold';
+
+export const TOKEN_COLOR_ALT = 'blue';
+export const TOKEN_SIZE_ALT = '15px';
+export const TOKEN_WEIGHT_ALT = 'normal';
