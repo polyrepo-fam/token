b83b31a6e95c5b3ed22c2452bea033ed4b3c2622
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 16:39:33 -0500

    fix: test change

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export const TOKEN_SIZE = '13px';
 export const TOKEN_WEIGHT = 'bold';
 
 export const TOKEN_COLOR_ALT = 'blue';
-export const TOKEN_SIZE_ALT = '15px';
+export const TOKEN_SIZE_ALT = '16px';
 export const TOKEN_WEIGHT_ALT = 'normal';
